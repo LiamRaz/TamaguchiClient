a459c7884961d06c67b859571c0f67c909d2b63a
Ariel Shoham <ariel.shoham400@gmail.com>
2021-02-07 15:53:52 +0200

    version 1.1.5 - Kalfon's work

--- TamaguchiClient/UI/Screens/PetStats.cs
+++ TamaguchiClient/UI/Screens/PetStats.cs
@@ -4,39 +4,54 @@ using System.Text;
 using System.Linq;
 using Tamaguchi.UI;
 using Tamaguchi.UI.Screens;
+using System.Threading.Tasks;
 
 
-//namespace Tamaguchi.UI.Screens
-//{
-//    class PetStats : Screen
-//    {
+namespace TamaguchiClient.UI.Screens
+{
+    class PetStats : Screen
+    {
 
-//        public PetStats() : base("Pet Stats")
-//        {
+        public PetStats() : base("Pet Stats")
+        {
 
-//        }
+        }
 
-//        public override void Show()
-//        {
-//            base.Show();
+        public override void Show()
+        {
+            base.Show();
 
-//            Console.WriteLine();
+            Console.WriteLine();
 
+            try
+            {
+                Task<List<object>> petStats = MainUI.client.GetPets();
+                Console.WriteLine("fetching your pets information...");
+                petStats.Wait();
 
-//            List<object> petStats = MainUI.currentPlayer.GetPetStats();
-//            if (petStats == null)
-//            {
-//                return;
-//            }
-//            ObjectsList list = new ObjectsList(" ", petStats);
-//            list.Show();
+                if (petStats.Result != null)
+                {
+                    ObjectsList list = new ObjectsList(" ", petStats.Result);
+                    list.Show();
+                }
+                else
+                {
+                    Console.WriteLine("No Data Found");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("oopsi doopi something went wrong or" + e.InnerException);
+                Console.WriteLine("enter any key to go back");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to go back");
+            Console.WriteLine();
+            Console.ReadKey();
 
-//            Console.WriteLine();
-//            Console.WriteLine("Press any key to go back");
-//            Console.WriteLine();
-//            Console.ReadKey();
+        }
 
-//        }
-
-//    }
-//}
+    }
+}
